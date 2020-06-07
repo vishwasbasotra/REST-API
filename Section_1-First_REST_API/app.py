d89dc6ca02f91657994bd061b01a01fa7a04b61b
Vishwas Basotra <brigu.sharma.9@gmail.com>
2020-06-08 02:36:19 +0530

    first api

---
+++ Section_1-First_REST_API/app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  8 01:32:58 2020
+
+@author: brigu
+"""
+# importing flask
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')    #http://www.google.com/
+def home():
+    return "hello world"
+
+app.run(port=5000)
